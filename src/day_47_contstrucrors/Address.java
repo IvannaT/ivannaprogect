69879f649adfccd4d86e97b09fe7a9c78c0cb18a
Oleg Tsetnar <oleg.cetnar@gmail.com>
2022-04-19 11:14:24 -0700

    added new method

--- src/day_47_contstrucrors/Address.java
+++ src/day_47_contstrucrors/Address.java
@@ -1,14 +1,29 @@
 package day_47_contstrucrors;
 
 public class Address {
-  private   String street;
+
+    private   String street;
+    private   String city;
+    private   String state;
+    private   String zipCode;
+    private   String country = "USA";
+//constructor- automatically called
+    public Address(){
+        System.out.println("Address constructor");
+        street= "123 unknown st";
+        city = "java";
+        state = "unknown";
+        zipCode = "OOO0";
+
+    }
+
 
     public String getStreet() {
         return street;
     }
 
     public String toString() {
-        return street +", "+ city + ", "+ state +", "+ zipCode;
+        return street +", "+ city + ", "+ state +" "+ zipCode;
     }
 
     public void setStreet(String street) {
@@ -46,10 +61,5 @@ public class Address {
     public void setCountry(String country) {
         this.country = country;
     }
-
-    private   String city;
-  private   String state;
-  private    String zipCode;
-  private String country = "USA";
 
 }
